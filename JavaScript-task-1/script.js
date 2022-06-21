859a35e9152866335dbcc81190bd7aab090ab8de
adenijialiuadeyemi <56898925+adenijialiuadeyemi@users.noreply.github.com>
2022-06-22 00:48:11 +0100

    Javascript Introduction

---
+++ JavaScript-task-1/script.js
@@ -0,0 +1,7 @@
+var username = "Adeniji ALiu Adeyemi";
+var height = "1.65m";
+var country = "Nigeria";
+
+console.log("Name: " + username);
+console.log("Height: " + height);
+console.log("Country: "+ country);
